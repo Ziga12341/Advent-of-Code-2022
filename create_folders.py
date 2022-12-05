825b62d1cf0edb9f3e6c92d924bb81b95f1a2c51
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2022-12-05 15:58:37 +0100

    Day 5, first part done

--- create_folders.py
+++ create_folders.py
@@ -1,7 +1,10 @@
 import os
+import shutil
 
 for day in range(1, 26):
     new_path = f"{os.getcwd()}\\Day {day}"
     if not os.path.exists(new_path):
         os.makedirs(new_path)
-    # print(newpath)
+        shutil.copy("input.txt", f"Day {day}/")
+        shutil.copy("second_part.py", f"Day {day}/")
+        shutil.copy("first_part.py", f"Day {day}/")
