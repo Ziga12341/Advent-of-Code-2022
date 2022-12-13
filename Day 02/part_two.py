4f3e74dd1069343829471d67fa19d2ad5b9a403a
Ziga Pregelj - Asus <ziga.pregelj@razum.si>
2022-12-13 23:18:46 +0100

    changed dirs names

--- Day 02/part_two.py
+++ Day 02/part_two.py
@@ -1,10 +1,11 @@
-# result for second part of Day 2
+# result for first part of Day 02 and preparation for second game output
 
-with open("output.txt", "r", encoding="utf-8") as file:
+new_file = open("output.txt", "w", encoding="utf-8")
+with open("input.txt", "r", encoding="utf-8") as file:
     # Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock
     # Opponent: A for Rock, B for Paper, and C for Scissors
     # My: X for Rock, Y for Paper, and Z for Scissors
-    second_game_total_score = 0
+    first_game_total_score = 0
 
     points = {"A": 1,
               "B": 2,
@@ -22,14 +23,24 @@ with open("output.txt", "r", encoding="utf-8") as file:
         "round": 3,
         "lose": 0}
 
+    # X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+    how_i_need_to_pay = {
+        "X": "lose",
+        "Y": "round",
+        "Z": "win"
+    }
+
     # loop through all file
     for game in file:
         game = game.split("\n")[0]
         game = tuple(game.split(" "))
         opponent, me = game
+        for his_play, my_play in results[how_i_need_to_pay[me]]:
+            if his_play == opponent:
+                new_file.write(f"{his_play} {my_play}\n")
 
         for result, options in results.items():
             if game in options:
-                second_game_total_score += points_for_game[result] + points[me]
+                first_game_total_score += points_for_game[result] + points[me]
 
-    print(second_game_total_score)
+print(first_game_total_score)
